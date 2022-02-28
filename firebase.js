aa1a90a130046f3875b9b06eacb649d8272522f0
Zubair Sidhu <sidhuzubair@gmail.com>
2022-02-27 23:46:42 -0800

    Make firebase sdk filename generic

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 
 require('dotenv').config();
 
-const credentials = require('./auth-dev-90d20-firebase-adminsdk-7k0yw-5c633a4fb2.json');
+const credentials = require('./firebase-adminsdk.json');
 
 admin.initializeApp({credential: admin.credential.cert(credentials)});
 
